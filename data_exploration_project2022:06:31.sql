dcf8542a66a189ac111d66b8681b20ce89047c53
EfetheAnalyst <106610524+Efesco17@users.noreply.github.com>
2022-06-23 20:11:36 +0100

    Update data_exploration_project2022:06:31.sql
    Exploratory Data Analysis Project on covid-19

--- data_exploration_project2022:06:31.sql
+++ data_exploration_project2022:06:31.sql
@@ -1,21 +1,27 @@
 use PortfolioProject;
-
+#1
 select *
 from PortfolioProject.coviddeaths
 where continent is not null and continent!=''
 order by location, date;
 
-# select data we are going to be using here
 
+#2
+-- DEMONSTRATING ABILITY TO CREATE BASIC QUERIES 
+	-- Select all countries metrics 
 select location,date,total_cases,new_cases,total_deaths,population
 from PortfolioProject.coviddeaths
 order by location, date;
 
-# looking at total cases v total deaths
-# shows the likelihood of dying if you contract covid in your country
+
+# 3
+-- DEMONSTRATING ABILITY TO PEFORM CALCULATIONNS AND USE ALIASES IN QUERIES
+	-- looking at total cases v total deaths
+		-- shows the likelihood of dying if you contract covid in your country
 select location,date,total_cases,population, (total_cases/population)*100 as Death_percentage
 from PortfolioProject.coviddeaths
-where location like '%states%'
+#where location like '%states%'
+where continent is not null and continent!=''
 order by location, date;
 
 # create view for deathpercentage per country
@@ -27,9 +33,9 @@ where location like '%states%'
 order by location, date;
 
 
-
-
-# looking at countries with highest infection rate compared to population 
+# 4 
+-- DEMONSTRATING ABILITY TO USE AGGREGATE FUNCTIONS AND USE GROUP BY STATEMENTS 
+	-- looking at countries with highest infection rate compared to population 
 select location,population,MAX(total_cases) as highestinfectioncount, max((total_cases/population))*100 as percentofpopulationinfected
 from PortfolioProject.coviddeaths
 group by location, population
@@ -46,9 +52,9 @@ group by location, population
 order by percentofpopulationinfected desc ;
 
 
-
-
-# showing the continents with the highest death counts
+# 5
+-- DEMONSTRATING ABILITY TO CONVERT DATATYPES USING CAST FUNCTION
+	-- showing the continents with the highest death counts
 select continent,MAX(cast(total_deaths as unsigned)) as highestdeathcount, max((total_deaths/population))*100 as percentofdead
 from PortfolioProject.coviddeaths
 where continent is not null and continent!=''
@@ -56,6 +62,7 @@ group by continent
 # where location like '%states%'
 order by highestdeathcount desc ;
 
+-- DEMONSTRATING ABILITY TO CREATE VIEWS
 create view highestdeathcount as 
 select continent,MAX(cast(total_deaths as unsigned)) as highestdeathcount, max((total_deaths/population))*100 as percentofdead
 from PortfolioProject.coviddeaths
@@ -65,8 +72,7 @@ group by continent
 order by highestdeathcount desc ;
 
 
-
-
+# 6 
 # showing countries with highest death count per population
 select location,MAX(cast(total_deaths as unsigned)) as highestdeathcount, max((total_deaths/population))*100 as percentofdead
 from PortfolioProject.coviddeaths
@@ -76,8 +82,8 @@ group by location, population
 order by highestdeathcount desc ;
 
 
+# 7
 # Global numbers
-
 select date,SUM(new_cases) as total_cases, SUM(cast(new_deaths as unsigned))as total_deaths, SUM(new_deaths)/SUM(new_cases)*100 as death_percentage
 from PortfolioProject.coviddeaths
 where continent is not null and continent!=''
@@ -86,10 +92,11 @@ order by 1,2;
 
 # looking at total population v vaccination
 
+
+# 8 
+-- DEMONSTRATING ABILITY TO CREATE & USE COMMON TABLE EXPRESSIONS and OVER & PARTITION BY CLAUSES
+	-- rolling people vaccinated way to add up or accumulate rows in a certain column
 use PortfolioProject;
-
-# rolling people vaccinated way to add up or accumulate rows in a certain column
-
 With PopvsVac (continent,location, date, population, new_vaccinations, rolling_people_vaccinated)
 as
 (
@@ -108,9 +115,9 @@ where dea.continent is not null and dea.continent!=''
 select *, (rolling_people_vaccinated/population)*100 as PopvsVacpercentage
 from PopvsVac;
 
-
-# temp table
-# declare the temp table
+-- DEMONSTRATING AILITY TO CREATE AND USE TEMP TABLES
+-- 	temp table
+-- 		declare/create the temp table
 
 
 create temporary table percentpopulationvaccinated
@@ -123,7 +130,7 @@ new_vaccinations numeric,
 Rolling_people_vaccinated numeric
 );
 
-# copy data into temp table
+-- copy data into temp table
 
 Insert into percentpopulationvaccinated
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as UNSIGNED))
@@ -135,8 +142,10 @@ join covidvaccinations vac
     and dea.date=vac.date
 where dea.continent is not null and dea.continent!='' 
 ;
-# Select data from the temporary table
+-- Select data from the temporary table
 
+
+ # 9
 select *, (rolling_people_vaccinated/population)*100 as PopvsVacpercentage
 from percentpopulationvaccinated;
 
